40cb3ce13691cc409fd99979b11b6fed9b1ebe53
CoolShivas <shivas2710cool00@gmail.com>
2024-06-01 02:08:26 +0530

    Getting the data instead of  Console to Cart or Bucket inside it  after clicking on Add To Cart Btn.

--- redux_react_cart/src/components/Cart/Cart.jsx
+++ redux_react_cart/src/components/Cart/Cart.jsx
@@ -1,15 +1,27 @@
 import CartItems from "./CartItems";
 import Card from "../UI/Card";
 import classes from "./Cart.module.css";
+import { useSelector } from "react-redux";
 
 const Cart = (props) => {
+
+    const cartProducts = useSelector((store) => store.cartBucket.items);
+
     return (
         <Card className={classes.cart}>
             <h2>Your Shopping Cart</h2>
             <ul>
-                <CartItems
+                {/* <CartItems
                     item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-                />
+                /> */}
+
+                {cartProducts.map((currItem) => {
+                    return <CartItems key={currItem.id}
+                        item={{ title: currItem.name, quantity: currItem.quantity, total: currItem.totalPrice, price: currItem.price }}
+                    ></CartItems>
+                })}
+                {/* Here, we are getting the title: currItem.name, quantity: currItem.quantity, total: currItem.totalPrice from the reducers fucntion from addItemsToCart */}
+
             </ul>
         </Card>
     );
