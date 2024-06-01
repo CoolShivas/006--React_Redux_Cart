7b60aabc685de4580aa82af64ba78a021dd63ddc
CoolShivas <shivas2710cool00@gmail.com>
2024-06-01 17:15:50 +0530

    Making the App Component lean and moving the useEffect data to come from store

--- redux_react_cart/src/App.jsx
+++ redux_react_cart/src/App.jsx
@@ -1,10 +1,15 @@
+import { sendCartData } from "./store";
 import Notification from "./components/UI/Notification";
 import Products from "./components/Shop/Products";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store";
+
+
+let initialNotify = true;
+// Here, preventing or stoping the error notification rendering first time ;
+
 
 function App() {
 
@@ -19,40 +24,14 @@ function App() {
 
 
   useEffect(() => {
-    const sendCartData = async () => {
-      // Here, we have use notification as loader while sending the request to server;
-      dispatch(uiActions.showNotify({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data! ",
-      }));
 
-      let response = await fetch(`https://reactreduxcart-a9eb0-default-rtdb.firebaseio.com/daliya.json`, {
-        method: "PUT",
-        body: JSON.stringify({
-          busket
-        }),
-      })
-      // Here, if there will we any error while connecting to server. We will catch the error;
-      if (!response.ok) {
-        throw new Error("Sending cart data is failed.")
-      }
-      // Here, if there will we no error while connecting to server. We will get the successful notification;
-      dispatch(uiActions.showNotify({
-        status: "success",
-        title: "Success",
-        message: "Sent cart data successfully",
-      }));
+    if (initialNotify) {
+      initialNotify = false;
+      return;
+      // Here, error notification stop at first rendering cycle;
+    }
 
-    };
-    // Here, catching the overall error;
-    sendCartData().catch(() => {
-      dispatch(uiActions.showNotify({
-        status: "error",
-        title: "Error",
-        message: "Sending cart data failed",
-      }));
-    })
+    dispatch(sendCartData(busket));
 
   }, [busket, dispatch]);
   // Here, we have use the method: "PUT", because the cart is dynamic if the user add more it will automatically updates whereas if we use post then the new item added;
